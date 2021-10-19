763b8d721531898a21f4ae3146aeaec155f64eb1
kalai <kalaiyarasi.ec19@bitsathy.ac.in>
2021-10-19 20:19:48 +0530

    port changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 80;
+const port = process.env.PORT || 3000;
 const User = require("./models/user");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
